8cdd062c8d8af00a95a2edc6215631ebffa6a789
Trond Einar Snekvik <trondesn@stud.ntnu.no>
2014-03-14 15:50:44 +0100

    added prefetcher.cc-code to elem.cc. Fuck this scons shit.

--- framework/elem.cc
+++ framework/elem.cc
@@ -139,3 +139,25 @@ XElem* ElemManager::addCombination(int d1, int d2)
 	
 	return xE;
 }
+
+///////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+ElemManager* manager;
+
+void prefetch_access(AccessStat stat)
+{
+  int nextFetch =  manager->findNextFetch((int)stat.mem_addr);
+  if(!in_cache((uint64_t)nextFetch))
+    {
+      issue_prefetch((uint64_t)nextFetch);
+    } 
+}
+
+void prefetch_complete(Addr addr)
+{
+}
+
+void prefetch_init(void)
+{
+	manager = new ElemManager();
+}
