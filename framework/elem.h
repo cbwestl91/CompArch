6b6b15948c29b45755690992994a31c0e5a51424
Trond Einar Snekvik <trondesn@stud.ntnu.no>
2014-03-14 12:31:21 +0100

    implemented previousActualCandidate

--- framework/elem.h
+++ framework/elem.h
@@ -10,7 +10,7 @@
 class Elem 
 {
 public:
-	long delta;
+	int delta;
 };
 
 
@@ -22,17 +22,16 @@ class XElem: public Elem
 {
 public:
 	
-	inline long getNext() { return candidate; };
+	inline int getNext() { return candidate; };
 	
 
 	/**
 	* apply actual result, and update score. Change candidate if score == 0
 	*/
-	void applyActualResult(long d3);
+	void applyActualResult(int d3);
 private:
-	long candidate;
+	int candidate;
 	uint8_t score;
-	
 };
 
 
@@ -49,21 +48,25 @@ public:
 	/**
 	tell the element the actual delta it got
 	*/
-	void previousActualCandidate(long d3);
+	void previousActualCandidate(int d3);
 
 	/**
 	return 0 if not found, and add combination if more room
 	*/
-	long getDelta(long d1, long d2);
+	int getDelta(int d1, int d2);
+	
+	ElemManager();
 	
 private:
 	/**
 	used by getDelta to add new combo
 	*/
-	void addCombination(long d1, long d2);
+	void addCombination(int d1, int d2);
 
 	uint16_t elemCount;
 	std::vector<YElem*> elements;
+
+	unsigned int mFetches;
 };
 
 
